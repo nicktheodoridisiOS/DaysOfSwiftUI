e4d9f3b42d9b3e0d295922ab19e90ae2670fd471
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-06-08 08:54:14 +0300

    Day3

---
+++ DaysOfSwiftUI/DaysOfSwiftUI/Views/Day3.swift
@@ -0,0 +1,28 @@
+//  Day1.swift
+//  8/6/23
+
+import SwiftUI
+
+struct Day3: View {
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(Color("rectcolor"))
+                .frame(width: 310 , height: 80)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color("strcolor"), lineWidth: 2)
+                )
+            Text("//Day3.swift")
+                .font(.system(size: 20,weight: .medium,design: .monospaced))
+                .foregroundColor(Color("compcolor"))
+        }
+        
+    }
+}
+
+struct Day3_Previews: PreviewProvider {
+    static var previews: some View {
+        Day3()
+    }
+}
