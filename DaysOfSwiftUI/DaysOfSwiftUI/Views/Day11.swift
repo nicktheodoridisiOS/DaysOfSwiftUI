c11890bc5a5c5da04431db12beeb5751b9132b16
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-06-16 14:04:27 +0300

    Day11View

---
+++ DaysOfSwiftUI/DaysOfSwiftUI/Views/Day11.swift
@@ -0,0 +1,50 @@
+//
+//  Day11.swift
+//  DaysOfSwiftUI
+//
+//  Created by Nick Theodoridis on 15/6/23.
+//
+
+import SwiftUI
+
+struct Day11: View {
+    var body: some View {
+        VStack{
+            
+            Spacer()
+            HStack(spacing:0){
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.red)
+                    .frame(width: 150 , height: 150)
+                Spacer()
+                    .frame(width: 20)
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.green)
+                    .frame(width: 150 , height: 150)
+            }
+            .padding(.horizontal)
+            HStack(spacing:0){
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.blue)
+                    .frame(width: 150 , height: 150)
+                Spacer()
+                    .frame(width: 20)
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.orange)
+                    .frame(width: 150 , height: 150)
+            }
+            .padding(.horizontal)
+            .padding(.top,10)
+            
+            
+            Spacer()
+           
+        }
+    }
+}
+
+struct Day11_Previews: PreviewProvider {
+    static var previews: some View {
+        Day11()
+    }
+}
