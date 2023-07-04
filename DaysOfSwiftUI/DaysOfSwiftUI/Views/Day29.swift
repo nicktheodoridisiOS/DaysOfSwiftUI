1d0d0171d171056d15c51841ef19bea2793afa8e
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-07-04 18:12:35 +0300

    Day29View

---
+++ DaysOfSwiftUI/DaysOfSwiftUI/Views/Day29.swift
@@ -0,0 +1,58 @@
+//
+//  Day29.swift
+//  DaysOfSwiftUI
+//
+//  Created by Nick Theodoridis on 3/7/23.
+//
+
+import SwiftUI
+
+struct Day29: View {
+    var body: some View {
+        VStack{
+            HStack(){
+                Button {
+                    
+                } label: {
+                    Image("icon")
+                        .resizable()
+                        .foregroundColor(.accentColor)
+                        .frame(width: 100,height: 100)
+                        .contextMenu(menuItems: {
+                            Button {
+                                
+                            } label: {
+                                Label("Copy", systemImage: "doc.on.doc")
+                            }
+                            
+                            Button {
+                                
+                            } label: {
+                                Label("Share", systemImage: "square.and.arrow.up")
+                            }
+                            
+                            Button {
+                                
+                            } label: {
+                                Label("Favourite", systemImage: "heart")
+                            }
+
+                        })
+                }
+            }
+            Spacer()
+        }
+        .frame(maxWidth: .infinity , alignment: .topLeading)
+        .padding()
+        .padding(.top , 20)
+        
+        
+     
+    }
+}
+
+struct Day29_Previews: PreviewProvider {
+    static var previews: some View {
+        Day29()
+    }
+}
