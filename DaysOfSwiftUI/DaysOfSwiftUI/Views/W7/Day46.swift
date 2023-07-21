7a084ff6fdfdd6724e0335538cd91612bec12140
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-07-21 09:53:44 +0100

    Day46View

---
+++ DaysOfSwiftUI/DaysOfSwiftUI/Views/W7/Day46.swift
@@ -0,0 +1,55 @@
+//
+//  Day46.swift
+//  DaysOfSwiftUI
+//
+//  Created by Nick Theodoridis on 20/7/23.
+//
+
+import SwiftUI
+
+
+struct Day46: View {
+    @State var material:Bool = false
+    
+    var body: some View {
+       
+        VStack(spacing: 100){
+            VStack{
+                VStack{
+                }
+                .frame(height: 250)
+                .frame(maxWidth: 250)
+                .background(material ? .ultraThinMaterial : .ultraThickMaterial)
+                .cornerRadius(30)
+                .padding()
+            }
+            .ignoresSafeArea()
+            .background(
+                Image("icon")
+                    .resizable()
+                    .frame(width: 250 ,height: 250)
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+            )
+        
+            
+            Text("Apply Material")
+                .foregroundColor(.white)
+                .onTapGesture {
+                    material.toggle()
+                }
+                .background{
+                    Capsule()
+                        .foregroundColor(.accentColor)
+                        .frame(width: 150, height: 40)
+                }
+          
+        }
+   
+    }
+}
+
+struct Day46_Previews: PreviewProvider {
+    static var previews: some View {
+        Day46()
+    }
+}
