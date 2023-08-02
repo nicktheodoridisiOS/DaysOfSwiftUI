137f47ecda51b800e6887cf312f2cdb3b9c80951
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-08-02 15:39:30 +0300

    Day58View

---
+++ DaysOfSwiftUI/DaysOfSwiftUI/Views/W9/Day58.swift
@@ -0,0 +1,43 @@
+//
+//  Day58.swift
+//  DaysOfSwiftUI
+//
+//  Created by Nick Theodoridis on 1/8/23.
+//
+
+import SwiftUI
+
+struct Day58: View {
+    
+    @State var angle: Angle  = Angle()
+    
+    var body: some View {
+        VStack{
+            
+            
+            Image(systemName: "swift")
+                .font(.system(size: 90,weight: .semibold,design: .default))
+                .foregroundColor(.orange)
+                .rotationEffect(angle)
+                .gesture(
+                    RotationGesture()
+                        .onChanged{ value in
+                            angle = value
+                        }
+                        .onEnded{ value in
+                            withAnimation(.spring()){
+                                angle = Angle(degrees: 0)
+                            }
+                            
+                        }
+                )
+            
+        }
+    }
+}
+
+struct Day58_Previews: PreviewProvider {
+    static var previews: some View {
+        Day58()
+    }
+}
