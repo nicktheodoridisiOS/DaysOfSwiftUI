8a2460f19e7d77926bd4c51d22214808f45e4992
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-08-24 19:00:46 +0300

    Day80View

---
+++ DaysOfSwiftUI/DaysOfSwiftUI/Views/W12/Tic-Tac-Toe/AIGameplayView.swift
@@ -0,0 +1,20 @@
+//
+//  AIGameplayView.swift
+//  DaysOfSwiftUI
+//
+//  Created by Nick Theodoridis on 23/8/23.
+//
+
+import SwiftUI
+
+struct AIGameplayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AIGameplayView_Previews: PreviewProvider {
+    static var previews: some View {
+        AIGameplayView()
+    }
+}
--- DaysOfSwiftUI/DaysOfSwiftUI/Views/W12/Tic-Tac-Toe/AIView.swift
+++ DaysOfSwiftUI/DaysOfSwiftUI/Views/W12/Tic-Tac-Toe/AIView.swift
@@ -9,9 +9,47 @@ import SwiftUI
 
 struct AIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ScrollView(showsIndicators: false){
+                VStack(spacing: 30){
+                    Image("AICover")
+                        .resizable()
+                        .scaledToFit()
+                    VStack(alignment: .leading, spacing: 5){
+                        Text("ABOUT AI")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                        Text("Artificial Intelligence (AI) is a field of computer science that deals with the design and development of systems capable of performing tasks that require human intelligence. The AI of this application (Tic Tac Toe) has been programmed to face any human intelligence. Are you capable of facing it?")
+                            .font(.system(size: 17))
+                    }
+                    .frame(maxWidth: .infinity,alignment:.topLeading)
+                    .padding(.horizontal,30)
+                    
+                    Spacer()
+                    NavigationLink(destination: AIGameplayView(), label: {
+                        HStack{
+                            Image(systemName: "poweroutlet.type.f.fill")
+                            Text("Play With AI")
+                                
+                                
+                        }
+                       
+                        
+                    })
+                    .buttonStyle(.bordered)
+                    .tint(.yellow)
+                    
+                    
+                }
+                
+            }
+            .navigationTitle("Play With AI")
+            
+        }
+        
     }
 }
+
 
 struct AIView_Previews: PreviewProvider {
     static var previews: some View {
